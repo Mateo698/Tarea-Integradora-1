c2fb10104053f97102c586e74b9a5da12f41e404
Mateo Rada <mateora73@gmail.com>
2020-09-17 15:55:36 -0500

    Javadoc added and program finalized

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -2,10 +2,16 @@ package model;
 
 public class Operations{
 	
-	private static int blackWork = 1300000;
-	private static int whiteWork = 2600000;
-	private static int painting = 980000;
+	private final static int blackWork = 1300000;
+	private final static int whiteWork = 2600000;
+	private final static int painting = 980000;
 	
+	/**
+	*Genera el total a pagar para cada establecimiento mas la mano de obra<br>
+	*<b>pre: </b> La lista de precios para la tienda debe haber sido incializada y declarada. <br>
+	*<b>post: </b> Se genera el total a pagar <br>
+	*@param HarwareStore Tienda a calcular precio total. HardwareStore[i] >= 0
+	*/
 	public static int priceForPlace(int[] HardwareStore){
 		int total = 0;
 		for(int i=0; i<HardwareStore.length; i++){
@@ -15,6 +21,14 @@ public class Operations{
 		return total;
 	}
 	
+	/**
+	*Genera el array con los mejores precios para cada producto de las 3 tiendas<br>
+	*<b>pre: </b> La listas de precios deben haber sido incializadas y declaradas con numeros naturales. <br>
+	*<b>post: </b> Devuelve la lista o array con los mejores precios de las 3 tiendas. <br>
+	*@param HC Precios en HomeCenter. HC[i]>=0
+	*@param FC Precios en la ferreteria del centro. FC[i]>=0
+	*@param FB Precios en la ferretreria del barrio. FB[i]>=0
+	*/
 	public static int[] bestPrice(int[] HC, int[] FC, int[] FB){
 		int[] best = new int[HC.length];
 		for(int i=0; i<best.length; i++){
@@ -27,12 +41,20 @@ public class Operations{
 			else if(FB[i]<HC[i] & FB[i]<FC[i]){
 				best[i]=FB[i];
 			}
-			System.out.println(best[i]);
+			
 		}
 		
 		return best;
 	}
 	
+	/**
+	*Genera un array con el index identificador que identifica donde se compra el producto al mejor precio.<br>
+	*<b>pre: </b> La listas de precios deben haber sido incializadas y declaradas con numeros naturales. <br>
+	*<b>post: </b> Devuelve la lista o array con los mejores precios de las 3 tiendas. <br>
+	*@param HC Precios en HomeCenter. HC[i]>=0
+	*@param FC Precios en la ferreteria del centro. FC[i]>=0
+	*@param FB Precios en la ferretreria del barrio. FB[i]>=0
+	*/
 	public static int[] bestIndex(int[] HC, int[] FC, int[] FB){
 		int[] bestIndex = new int[HC.length];
 		for(int i=0; i<bestIndex.length; i++){
@@ -45,8 +67,53 @@ public class Operations{
 			else if(FB[i]<HC[i] & FB[i]<FC[i]){
 				bestIndex[i]=3;//Y el 3 para la Ferreteria del barrio
 			}
-			System.out.println(bestIndex[i]);
+			
 		}
 		return bestIndex;
 	}
+	
+	/**
+	*Genera el precio a pagar de todos los productos a su mejor precio junto con el valor del domicilio.<br>
+	*<b>pre: </b> Los precios para cada tienda deben haber sido declarados. <br>
+	*<b>post: </b> Devuelve el precio a pagar por los productos. <br>
+	*@param bestPrices Lista de los mejores precios. bestPrices[i]>=0
+	*@param location Entero que indica la ubicacion del inmueble. 3>=location>=1
+	*/
+	public static int finalPrice(int[] bestPrices, int location){
+		int total= 0;
+		for(int i=0; i<bestPrices.length; i++){
+			total += bestPrices[i];
+		}
+		switch(location){
+			case 1:
+				if(total<80000){
+					total += 120000;
+				}
+				else if(total<300000){
+					total += 28000;
+				}			
+			break;
+			
+			case 2:
+				if(total<80000){
+					total += 50000;
+				}
+			break;
+			
+			case 3:
+				if(total<80000){
+					total += 120000;
+				}
+				else if(total<300000){
+					total += 55000;
+				}	
+			break;
+			
+			default:
+			
+		}
+		
+		return total;
+	}
+	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,15 +3,39 @@ import model.*;
 import java.util.Scanner;
 public class Main{
 	
-
-	
+	/**
+	*Lee la cantidad de materiales por consola<br>
+	*<b>pre: </b> La variable de la cantidad debe estar inicializada. <br>
+	*<b>post: </b> Devuelve la cantidad de materiales para la obra. <br>
+	*@param in debe haber sido inicializado
+	*/
 	public static int askQuantity(Scanner in){
 		System.out.println("Ingrese la cantidad de materiales que desea ingresar");
 		int quantity = in.nextInt();
-		in.nextLine();
+		in.nextLine();/**   */
 		return quantity;
 	}
 	
+	
+	/**
+	*Lee la ubicacion del inmueble<br>
+	*<b>pre: </b> La variable location debe haber sido inicializada. <br>
+	*<b>post: </b> Devuelve un entero el cual indica la ubicacion del inmueble. <br>
+	*@param in debe haber sido inicializado.
+	*/
+	public static int askLocation(Scanner in){
+		System.out.println("Ingrese 1 si su inmueble se encuentra en el norte, 2 si esta en el centro y 3 si esta en el sur");
+		int location = in.nextInt();
+		in.nextLine();
+		return location;
+	}
+	
+	/**
+	*Lee los precios para un producto en las 3 ferreterias y su utilizacion<br>
+	*<b>pre: </b> El array auxiliar que va a capturar el array generado que se usara para distribuir los datos en los respectivos arrays debe estar inicializado. <br>
+	*<b>post:</b> Devuelve un array con los datos necesarios. <br>
+	*@param in debe haber sido inicializado
+	*/
 	public static String[] askForMaterials(Scanner in){
 		String[] material = new String[5];
 		System.out.println("Ingrese el nombre del producto");
@@ -28,7 +52,14 @@ public class Main{
 	
 	}
 	
+	/**
+	*Calcula y muestra los datos del problema tales como los precios, mejores precios y valores a pagar<br>
+	*<b>pre: </b>  <br>
+	*<b>post: </b> Muestra los mejores precios y los precios totales a pagar. <br>
+	*@param in Scanner debe haber sido declarado.
+	*/
 	public static void showData(Scanner in){
+		int location = askLocation(in);
 		int quantity = askQuantity(in);
 		String[] materialsName = new String[quantity];
 		int[] priceHomeCenter = new int[quantity];
@@ -44,9 +75,9 @@ public class Main{
 			priceFB[i] = Integer.parseInt(material[3]);
 			utility[i] = Integer.parseInt(material[4]);
 		}
-		System.out.println("El precio total para HomeCenter es " + Operations.priceForPlace(priceHomeCenter));
-		System.out.println("El precio para la ferreteria del centro es "+ Operations.priceForPlace(priceFC));
-		System.out.println("El precio para la ferreteria del barrio es "+ Operations.priceForPlace(priceFB));
+		System.out.println("El precio total para HomeCenter es con la mano de obra " + Operations.priceForPlace(priceHomeCenter));
+		System.out.println("El precio para la ferreteria del centro es con la mano de obra "+ Operations.priceForPlace(priceFC));
+		System.out.println("El precio para la ferreteria del barrio es con la mano de obra "+ Operations.priceForPlace(priceFB));
 		
 		int[] bestPrice = new int[quantity];
 		int[] bestIndex = new int[quantity];
@@ -70,9 +101,20 @@ public class Main{
 				default:
 			}
 		}
+		int total = Operations.finalPrice(bestPrice,location);
+		System.out.println("El precio total a pagar por los materiales y el domicilio "+total);
 		
+		System.out.println("A continuacion se muestran los materiales usados para la obra junto con su utilidad(utilizacion)");
 		for(int i=0; i<quantity; i++){
-			System.out.println(materialsName[i] + " " + priceHomeCenter[i] + " " + priceFC[i] + " " + priceFB[i]);
+			if(utility[i]==1){//obra negra
+				System.out.println(materialsName[i]+" Obra negra");
+			}
+			else if(utility[i]==2){//obra blanca
+				System.out.println(materialsName[i]+" Obra blanca");
+			}
+			else{//pintura
+				System.out.println(materialsName[i]+" Pintura");
+			}
 			
 		}
 	}
